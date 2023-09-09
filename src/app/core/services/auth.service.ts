d16c2768b018740403458c1cb99ea544623ebea2
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2023-09-09 15:14:25 -0300

    Hands-on 11

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
 import { Observable, map, catchError, throwError } from 'rxjs'
 
 @Injectable({
@@ -8,12 +9,14 @@ import { Observable, map, catchError, throwError } from 'rxjs'
 export class AuthService {
   private url: string = 'http://localhost:3000'
 
-  constructor (private http: HttpClient) { }
+  constructor (private http: HttpClient, private router: Router) { }
 
   public sign (payload: { email: string, password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
       map((res) => {
-        return console.log(res)
+        localStorage.removeItem('access_token')
+        localStorage.setItem('access_token', res.token)
+        return this.router.navigate(['admin'])
       }),
       catchError((err) => {
         if (err.error?.message) return throwError(() => err.error.message)
@@ -21,5 +24,10 @@ export class AuthService {
         return throwError(() => 'No momento não estamos conseguindo validar estes dados, tente novamente mais tarde.')
       })
     )
+  }
+
+  public logout () {
+    localStorage.removeItem('access_token')
+    return this.router.navigate([''])
   }
 }
